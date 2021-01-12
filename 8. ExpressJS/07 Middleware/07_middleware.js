95cb999a64effc0fe3160e3fb47ee5e564cb7604
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2021-01-13 01:32:51 +0500

    Add files via upload

---
+++ 8. ExpressJS/07 Middleware/07_middleware.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const app = express();
+
+function log(req, res, next) {
+    console.log(new Date(), req.method, req.url);
+    next();
+}
+
+function secondMiddleware(req, res, next) {
+    // res.send() //me chahu to logic k behave pr middleware s bh send karsakta hon. or end bh karsakta yahi
+    console.log('Second Middileware');
+    next(); // to say this middleware is end
+}
+
+app.get('/', log, secondMiddleware, function (req, res) {
+    res.send('Express Works');
+});
+
+app.get('/error', function (req, res, next) {
+    throw new Error('Testing Err');
+});
+
+function notDefineRoute(req, res, next) {
+    res.send('404 not Found');
+}
+app.use(notDefineRoute);
+
+
+function errorHandler(err, req, res, next) {
+    if(err) {
+        res.send('Errror: ' + err)
+    }
+}
+app.use(errorHandler);
+
+
+app.listen(3000, function () {
+    console.log(`Express Server Started on: http://localhost:3000`);
+});
